9b5cb079e346d505bc741591e5a85094e1cadb89
cylia <cyliainfoamara22@gmail.com>
2025-11-28 10:39:46 +0100

    Add files via upload

---
+++ cyliaoam.py
@@ -0,0 +1,16 @@
+def saluer(nom):
+    print(f"Bonjour {nom} !")
+
+nom = input("Quel est ton nom? : ")
+
+while True:
+        nombre = int(input("Choisis un chiffre entre 1 et 10 : "))
+        if 1 <= nombre <= 10:
+             break
+        else:
+            print("Erreur : le chiffre doit être entre 1 et 10.")
+
+saluer(nom)
+
+for i in range(1, nombre + 1):
+    print(f"Python c'est cool !")
